9dbb294a90db21257c22d165b0aea9aa85f7d692
Galen Warren <galen@cvillewarrens.com>
2015-09-21 08:28:31 -0400

    incremental

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import koa from "koa";
+import {} from "GalenWarren/node-odata-core";
 
 export function odata( options ) {
 
@@ -6,6 +7,8 @@ export function odata( options ) {
 
   // handle requests
   app.use( function *() {
+    this.body =
+
     this.body = `request for path=${this.request.path} and querystring=${this.request.querystring}`;
   });
 
